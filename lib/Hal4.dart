c6dbdae69df9e305a272da54a9c58c1e2044bdfc
hasanulfitri27 <hasanulfitri77@gmail.com>
2021-02-09 16:41:43 +0700

    tugas
    Signed-off-by: hasanulfitri27 <hasanulfitri77@gmail.com>

--- lib/Hal4.dart
+++ lib/Hal4.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_apptest1/Hal7.dart';
 import 'package:http/http.dart' as http;
 
 class Hal4 extends StatefulWidget {
@@ -97,7 +98,10 @@ class Hal4State extends State<Hal4> {
                             FlatButton(
                               child: const Text('LIHAT DETAIL'),
                               onPressed: () {
-                                /* ... */
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Hal7()));
                               },
                             ),
                             FlatButton(
--- lib/NavBar.dart
+++ lib/NavBar.dart
@@ -4,6 +4,7 @@ import 'package:flutter_apptest1/Hal1.dart';
 import 'package:flutter_apptest1/Hal2.dart';
 import 'package:flutter_apptest1/Hal3.dart';
 import 'package:flutter_apptest1/Hal4.dart';
+import 'package:flutter_apptest1/Hal7.dart';
 
 class NavBar extends StatefulWidget {
   @override
@@ -13,7 +14,13 @@ class NavBar extends StatefulWidget {
 class _NavBarState extends State<NavBar> {
   int selectedpage = 0;
 
-  final _halaman = [Hal1(), Hal2(), Hal3(), Hal4()]; //listing of all 5 page
+  final _halaman = [
+    Hal1(),
+    Hal2(),
+    Hal3(),
+    Hal4(),
+    Hal7()
+  ]; //listing of all 5 page
 
   final bgcolor = [
     Colors.green,
